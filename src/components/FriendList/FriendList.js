9b13b22822f0d2bf5b25a6bd5e1e65d491c11afd
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-08-18 19:53:39 +0300

    finished

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,11 +1,12 @@
+import React from 'react';
 import { List, Desc, Span, Items } from "./FriendList.styled";
 
 export const FriendList = ({ items }) => {
   return (
     <Items className="friend-list">
       {items.map(item => (
-        <List key={item.id} isOnline={item.isOnline}>
-          <Span className="isOnline">{item.isOnline}</Span>
+        <List key={item.id}>
+          <Span isOnline={item.isOnline} />
           <img src={item.avatar} alt="User avatar" className="avatar" width="100" />
           <Desc className="name">{item.name}</Desc>
         </List>
--- src/components/FriendList/FriendList.styled.js
+++ src/components/FriendList/FriendList.styled.js
@@ -4,35 +4,25 @@ export const List = styled.li`
   display: flex;
   align-items: center;
   row-gap: 15px;
-width: 300px;
-background-color: white;
-border: 1px solid black;
-
-
-  /* Use the css function correctly */
-  ${props =>
-    props.isOnline &&
-    css`
-      .isOnline {
-        background-color: green;
-      }
-    `}
+  width: 300px;
+  background-color: white;
+  border: 1px solid black;
 `;
 
 export const Desc = styled.p`
   margin-left: 10px;
-`
+`;
 
 export const Span = styled.span`
-width: 10px;
+  width: 10px;
   height: 10px;
   border-radius: 50%;
-  background-color: red;
+  background-color: ${props => (props.isOnline ? "green" : "red")};
   margin-right: 5px;
 `;
 
 export const Items = styled.ul`
-display: flex;
-    gap: 13px;
-    flex-direction: column;
+  display: flex;
+  gap: 13px;
+  flex-direction: column;
 `;
