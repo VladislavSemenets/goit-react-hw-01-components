ebea5483e6ae8b563a6ec9267cb17e6ceffc908c
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-08-21 16:46:44 +0300

    fixed the mistakes

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,13 @@
 import { Container, Section, List, ListItem, Title, Label, Percentage, Box } from "./Statistics.styled.js";
 
-export const Statistics = ({items}) => {
+export const Statistics = ({ title, items }) => {
   const colors = ["red", "blue", "green", "orange"]; // список різних кольорів
 
   return (
     <Container>
       <Section className="statistics">
+        {title && <Title>{title}</Title>}
         <List className="stat-list">
-          <Title>UPLOAD STATS</Title>
           <ListItem className="item">
             {items.map((item, index) => (
               <Box key={item.id} style={{ backgroundColor: colors[index % colors.length] }}>
